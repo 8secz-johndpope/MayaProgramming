ae3974c8b4ea4636a75a62e0813d728df3c873a8
Hare8563 <haresaku1024@gmail.com>
2014-12-20 14:33:33 +0900

    2014/12/20 Maya公式Exampleを追加　

---
+++ ShaderPlugin/lambertShader/lambertShader/StdAfx.cpp
@@ -0,0 +1,14 @@
+//
+//  StdAfx.cpp
+//  lambertShader
+//
+//  Created by Tetsushi on 2014/12/20.
+//    Copyright (c) 2014年 Tetsushi. All rights reserved.
+//
+
+//-----------------------------------------------------------------------------
+//------ StdAfx.cpp : source file that includes just the standard includes
+//------  StdAfx.pch will be the pre-compiled header
+//------  StdAfx.obj will contain the pre-compiled type information
+//-----------------------------------------------------------------------------
+#include "StdAfx.h"
