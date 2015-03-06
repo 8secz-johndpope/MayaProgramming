e1824bfdec2f3b07f06e6d69d6a0e3a93bfdc647
Tetsushi <tetsushi2@Tetsushi-no-iMac.local>
2015-03-06 22:54:41 +0900

    GUI完成

--- Python/keyRotationWithUI.py
+++ Python/keyRotationWithUI.py
@@ -31,7 +31,7 @@ def createUI(pWindowTitle, pApplyCallback):
     cmds.separator(height=10, style="none")
     #第四行目
     cmds.separator(height=10, style="none")
-    cmds.button(label="Apply", command=functools.partical(pApplyCallback, startTimeField, endTimeField, targetAttributeField))
+    cmds.button(label="Apply", command=functools.partial(pApplyCallback, startTimeField, endTimeField, attributeField))
     
     def cancelCallBack(*pArgs):
             if cmds.window(windowID, exists=True):
@@ -40,14 +40,39 @@ def createUI(pWindowTitle, pApplyCallback):
     cmds.showWindow()
 
    
+def keyFullRotation(pObjectName, pStartTime, pEndTime, pTargetAttribute):
+    #objectName: 対象にするオブジェクト
+    #time: 時間の範囲、タプル
+    #attribute: カット, 追加する対象のアトリビュート
+    cmds.cutKey(pObjectName, time=(pStartTime, pEndTime), attribute=pTargetAttribute)
+    #キーの追加
+    cmds.setKeyframe(pObjectName, time=pStartTime, attribute=pTargetAttribute, value=0)
+    cmds.setKeyframe(pObjectName, time=pEndTime, attribute=pTargetAttribute, value=360)
+    #キーの選択
+    cmds.selectKey(pObjectName, time=(pStartTime, pEndTime), attribute=pTargetAttribute, keyframe=True)
+    #キータンジェントの変更
+    cmds.keyTangent(inTangentType="linear", outTangentType="linear")  
+ 
+
 def applyCallback(pStartTimeField, pEndTimeField, pTargetAttributeField, *pArgs):
     startTime = cmds.intField(pStartTimeField, query=True, value=True)
     endTime = cmds.intField(pEndTimeField, query=True, value=True)
-    targetText = cmds.textField(pTargetAttributeField, text=True)
+    targetText = cmds.textField(pTargetAttributeField, query=True, text=True)
     
-    print "startTime: %d\nendTime: %d\ntargetText: %s"%(startTime, endTime, targetText)
     
-createUI("MyTitle", applyCallback )
+    selectionList = cmds.ls(selection=True, type="transform")
+    
+    if len(selectionList) >= 1:
+        #print "Selected items: %s"%selectionList
+        for objectName in selectionList:
+           # objectTypeResult = cmds.objectType(objectName)
+           # print "%s type: %s"%(objectName, objectTypeResult)
+           keyFullRotation(objectName, startTime, endTime, targetText)
+           
+    else:
+        print "Please select at lease one object"
+    
+createUI("MyTitle", applyCallback)
    
    
    
